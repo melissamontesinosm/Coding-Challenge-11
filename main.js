ef05c19e35def546748fcd5bc196f41ac14a042e
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-10-27 13:53:02 -0400

    Calculate and update total price dynamically based on product and quantity

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 
 // Task 2: Add Event Listener for Product Selection
 
-const productSelector = document.getElementById('product-selector');
-const quantityInput = document.getElementById('quantity');
-const totalPriceElement = document.getElementById('total-price');
-const placeOrderButton = document.getElementById('place-order');
-const orderSummary = document.getElementById('order-summary'); 
+const productSelector = document.getElementById('product-selector');// Display Product Selector
+const quantityInput = document.getElementById('quantity'); // Display Quantity Input
+const totalPriceElement = document.getElementById('total-price'); // Display Total Price
+const placeOrderButton = document.getElementById('place-order'); // Display Place Order Button
+const orderSummary = document.getElementById('order-summary'); // Display Order Summar
+
+
+// Task 3: Calculate Total Price Dynamically
+
+function updateTotalPrice() {
+    const productPrice= parseFloat(productSelector.value); // Converting a non float value to a float(with decimal)
+    const quantity = parseInt(quantityInput.value); // Converting a on integer number to an integer
+    const totalPrice = productPrice * quantity; // Calculating total price
+
+    totalPriceElement.textContent = totalPrice.toFixed(2); // Updating total price
+}
